0a592ac0565d98d02080987adb95dd4b6cc61324
Draken <131387159+dragonx943@users.noreply.github.com>
2023-09-02 08:54:51 +0700

    Update setup-nodejs18.sh

--- setup-nodejs18.sh
+++ setup-nodejs18.sh
@@ -28,6 +28,10 @@ x86_64)
         archurl="x64" ;;
 esac
 curl -fLO https://github.com/dragonx943/alpine-node/releases/download/v18/node-v18-linux-${archurl}.tar.gz
+echo "W: Installing some important packages..."
+apk add binutils-gold gnupg linux-headers bash libgcc libstdc++ build-base cairo-dev jpeg-dev pango-dev freetype-dev giflib-dev gcc g++ make pkgconfig python3
+sleep 3
+clear
 echo "W: Installing nodejs & npm..."
 tar -xzf node-v18-linux-${archurl}.tar.gz -C /usr --strip-components=1 --no-same-owner
 echo "W: Cleaning up..."
